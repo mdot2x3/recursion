3ad9fcb61013c5a5a71b80628c744674f113a4f0
mdot2x3 <mdot2x3@gmail.com>
2025-09-22 13:18:59 -0700

    feat: add industry standard merge sort build
    add alternate merge sort code layout that has
    better time complexity, uses less memory, and is
    the approach more likely used in production code.

--- app.js
+++ app.js
@@ -59,3 +59,70 @@ function mergeSort(array) {
 }
 
 console.log(mergeSort([105, 79, 100, 110]));
+
+//----------------------------------------------
+
+// alternate merge sort (sorts in-place, memory efficient)
+var mergeSort = function (array, p, r) {
+  if (p >= r) {
+    return;
+  }
+
+  var q = Math.floor((p + r) / 2);
+  mergeSort(array, p, q);
+  mergeSort(array, q + 1, r);
+  merge(array, p, q, r);
+};
+
+// takes in an array that has two sorted subarrays,
+// from [p..q] and [q+1..r], and merges the array
+var merge = function (array, p, q, r) {
+  var lowHalf = [];
+  var highHalf = [];
+
+  var k = p;
+  var i;
+  var j;
+  for (i = 0; k <= q; i++, k++) {
+    lowHalf[i] = array[k];
+  }
+  for (j = 0; k <= r; j++, k++) {
+    highHalf[j] = array[k];
+  }
+
+  k = p;
+  i = 0;
+  j = 0;
+
+  // repeatedly compare the lowest untaken element in
+  // lowHalf with the lowest untaken element in highHalf
+  // and copy the lower of the two back into array
+  while (i < lowHalf.length && j < highHalf.length) {
+    if (lowHalf[i] <= highHalf[j]) {
+      array[k] = lowHalf[i];
+      i++;
+    } else {
+      array[k] = highHalf[j];
+      j++;
+    }
+    k++;
+  }
+  // once one of lowHalf and highHalf has been fully copied
+  // back into array, copy the remaining elements from the
+  // other temporary array back into the array
+  while (i < lowHalf.length) {
+    array[k] = lowHalf[i];
+    i++;
+    k++;
+  }
+
+  while (j < highHalf.length) {
+    array[k] = highHalf[j];
+    j++;
+    k++;
+  }
+};
+
+var array = [14, 7, 3, 12, 9, 11, 6, 2];
+mergeSort(array, 0, array.length - 1);
+console.log("Array after sorting: " + array);
